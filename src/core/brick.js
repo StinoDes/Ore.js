ea0579f5eecc32be3e028d12f5614b1ed425c003
Stijn Tytgat <stinodesigns@gmail.com>
2016-11-09 15:50:45 +0100

    Added lifecycle functions (passable through initial config)

--- src/core/brick.js
+++ src/core/brick.js
@@ -1,34 +1,95 @@
 const brick = (name, c) => (() => {
 
   const name = name,
-    configuration = c,
+
+    /**
+     * Lifecycle methods
+     */
+    setConfiguration = config => {
+      const lifecycles = {
+          init:         config.init,
+          willMount:    config.willMount,
+          didMount:     config.didMount,
+          willBuild:    config.willBuild,
+          build:        config.build,
+          didBuild:     config.didBuild,
+          willUnmount:  config.willUnmount
+        },
+        lifecycleWrapper = fn => args => fn ? fn.apply(configuration, args) : null,
+        init = lifecycleWrapper(lifecycles.init),
+        willMount = lifecycleWrapper(lifecycles.willMount),
+        didMount = lifecycleWrapper(lifecycles.didMount),
+        willBuild = lifecycleWrapper(lifecycles.willBuild),
+        build = lifecycleWrapper(lifecycles.build),
+        didBuild = lifecycleWrapper(lifecycles.didBuild),
+        willUnmount = lifecycleWrapper(lifecycles.willUnmount)
+
+      delete config.init,
+        config.willMount,
+        config.didMount,
+        config.willBuild,
+        config.build,
+        config.didBuild,
+        config.willUnmount
+
+      return {
+        init,
+        willMount,
+        didMount,
+        willBuild,
+        build,
+        didBuild,
+        willUnmount,
+        configuration: config,
+      }
+
+    },
+    {
+      configuration,
+      init,
+      willMount,
+      didMount,
+      build,
+      willBuild,
+      didBuild,
+      willUnmount
+    } = setConfiguration(c),
+
+    /**
+     * Transactional methods.
+     */
     refs = {
       virtual: null,
       actual: null
     },
-    /**
-     * Transactional methods.
-     */
-    renderVirtual = config => configuration.build(config),
+    renderVirtual = config => build(config),
     compare = () => {
       return {}
     },
     transact = comparation => {
-      return refs.actual
+      const mineral = refs.actual
         .labor(comparation)
+      didBuild([configuration])
+      return mineral
     },
 
     /**
      * Building methods.
      */
-    build = (config, children) => {
+    api = (config, children) => {
+      !refs.virtual ? willMount([configuration]) : willBuild([configuration])
+
       refs.virtual = renderVirtual({
         ...configuration,
         ...config,
         append: children
       })
+
+      //todo delete
+      refs.actual = null
       if (!refs.actual) {
         refs.actual = refs.virtual
+        didMount([configuration])
         return refs.virtual
       }
       else
@@ -38,10 +99,11 @@ const brick = (name, c) => (() => {
     /**
      * @return {boolean} - returns true.
      */
-  build.isBrick = () => true
+  api.isBrick = () => true
 
 
-  return build
+
+  return api
 
 })()
 
